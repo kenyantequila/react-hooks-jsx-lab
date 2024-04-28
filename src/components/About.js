280feecfd667164259ccfa0b836761f056d6ba74
kenyantequila <abdishakur431@gmail.com>
2024-04-28 23:15:24 +0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { image } from "../data/data";
+
 
 function About() {
   return <div>About</div>;
 }
 
 export default About;
+
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+
 
 function Home() {
   // update the JSX being returned!
